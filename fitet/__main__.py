d7cd2e549bfa26366cb87602422e9a0e656537a1
SamueleFacenda <samuele.facenda@gmail.com>
2023-11-15 10:03:34 +0100

    Improve main versability
    Signed-off-by: SamueleFacenda <samuele.facenda@gmail.com>

--- fitet/__main__.py
+++ fitet/__main__.py
@@ -2,16 +2,27 @@ from . import FitetParser, Player
 import sys
 
 def main():
-    dump_path = None # Default dump path
-    if len(sys.argv) > 1:
-        dump_path = sys.argv[1]
-    else:
-        dump_path = "fitet/matches.json"
+    dump_path = "fitet/matches.json"
+        
+    search_players = [x for x in sys.argv[1:] if '--dump-path=' not in x]
+    dump_list = [x for x in sys.argv[1:] if '--dump-path=' in x]
+    if dump_list:
+        dump_path = dump_list[0].split('=')[1]
+    
         
     parser = FitetParser(dump_path)
     parser.update(["Trentino"])
     print(len(parser.matches))
-    for match in Player.get("Facenda Samuele").matches: print(match)
-
+    
+    for player in search_players:
+        matches = Player.get(player).matches
+        print()
+        print('#'*(len(player)+6))
+        print('##' ,player, '##')
+        print('#'*(len(player)+6))
+        print('\n'.join([str(x) for x in matches]))
+        
+        
+        
 if __name__ == "__main__":
     main()
