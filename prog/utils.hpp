09711aa3bda41f5c23b6c9a20d5728e36ede2e74
SamueleFacenda <samuele.facenda@gmail.com>
2023-11-21 10:09:03 +0100

    Add multiple file exercises
    Signed-off-by: SamueleFacenda <samuele.facenda@gmail.com>

--- prog/utils.hpp
+++ prog/utils.hpp
@@ -3,5 +3,9 @@
 
 void crypt(char* word, int key);
 void decrypt(char* word, int key);
+char* readFile(char* name);
+void extend(char* &arr, int& len);
+char* getUpper(char* str, int i=0,int found=0);
+bool isEmail(char* mail);
 
 #endif
