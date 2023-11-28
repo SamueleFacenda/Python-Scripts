f6355df0db9c1da9ec4961c7f04431ea6f25b388
SamueleFacenda <samuele.facenda@gmail.com>
2023-11-28 10:42:23 +0100

    Add queue exercise cpp
    Signed-off-by: SamueleFacenda <samuele.facenda@gmail.com>

--- prog/stack_and_queue.cpp
+++ prog/stack_and_queue.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <cstdlib>
 #include <fstream>
-#include "pila.h"
-#include "utils.hpp"
+#include "lib/pila.h"
+#include "lib/utils.hpp"
+#include "lib/coda.h"
 // # define int unsigned long long
 
 using namespace std;
@@ -37,15 +38,48 @@ void putQueueInFile(char* name) {
         pila_top(tmp);
         out << tmp << endl;
     } while(pila_pop());
+    pila_deinit();
+}
+
+bool areBalanced(char* in) {
+    pila_init();
+    for(int i=0; in[i]; i++) {
+        switch(in[i]) {
+            case '(':
+                pila_push(0);
+                break;
+            case ')':
+                if (!pila_pop())
+                    return false;
+                break;
+        }
+    }
+    bool out = !pila_pop();
+    pila_deinit();
+    return out;
+}
+
+void invertiPila() {
+    coda_init();
+    int tmp;
+    do {
+        if (pila_top(tmp)) 
+            coda_enqueue(tmp);
+    } while (pila_pop());
+    
+    do {
+        if (coda_first(tmp))
+            pila_push(tmp);
+    } while (coda_dequeue());
+    coda_deinit();
 }
 
 signed main(int argc, char** argv) {
     checkArgs(2,argc);
     readIntToQueue(argv[1]);
+    invertiPila();
     putQueueInFile(argv[2]);
-
-
-    return 0;
+    
 }
 
 
