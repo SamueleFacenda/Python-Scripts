872cadffe15031a529aa614ad8ba679fcb0bde81
SamueleFacenda <samuele.facenda@gmail.com>
2024-01-13 10:48:38 +0100

    Move univr-timetable to coseWeb
    Signed-off-by: SamueleFacenda <samuele.facenda@gmail.com>

--- univr-scraper/parser.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// read the href from the file
-$fp = fopen('href.txt', 'r');
-$last_time = fgets($fp);
-$href = fgets($fp);
-fclose($fp);
-
-?>
--- univr-scraper/redirecter.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-include_once 'parser.php';
-
-// redirect to the href page
-header("Location: $href");
